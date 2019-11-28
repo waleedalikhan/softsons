07d10db44c1dea135f467a9a30982703b37810dc
Waleed <waleed@maktop.local>
2019-11-28 01:32:23 -0800

    some changes

--- res/js/script.js
+++ res/js/script.js
@@ -1,17 +1,25 @@
 $(function() {
-    $(' .navbar-toggler ').click(function() {
-        $(' .navbar-toggler ').toggleClass(' change ')
-    });
+  $(" .navbar-toggler ").click(function() {
+    $(" .navbar-toggler ").toggleClass(" change ");
+  });
 });
 
 $(window).scroll(function() {
-    let position = $(this).scrollTop()
-    if(position >= 160){ 
-        $('nav').addClass('custom-navbar')
-        .removeClass('nav-menu');
-    } else{
-        $('nav').addClass('nav-menu')
-        .removeClass('custom-navbar');
-    }
-})
+  let position = $(this).scrollTop();
+  if (position >= 160) {
+    $("nav")
+      .addClass("custom-navbar")
+      .removeClass("nav-menu");
+  } else {
+    $("nav")
+      .addClass("nav-menu")
+      .removeClass("custom-navbar");
+  }
+});
 
+$(".nav-link").click(function() {
+  $(this)
+    .addClass("active")
+    .siblings()
+    .removeClass("active");
+});
